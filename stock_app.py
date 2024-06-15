dc57b2a4cc7801c224646c9ecb86a6abb7426eaf
Danni Liu <123027766+Danni-Dan-Liu@users.noreply.github.com>
2024-06-15 19:24:29 +1000

    Update stock_app.py

--- stock_app.py
+++ stock_app.py
@@ -9,14 +9,20 @@ Shown are the stock **closing price** and ***volume*** of Nvidia!
 
 """)
 
+# User input for the ticker symbol
+tickerSymbol = st.text_input("Enter a stock ticker symbol (e.g., NVDA):", "NVDA")
 
-#define the ticker symbol
-tickerSymbol = "NVDA"
+# User input for the start and end dates
+start_date = st.date_input("Enter Start Date", pd.to_datetime("2014-05-31"))
+end_date = st.date_input("Enter End Date", pd.to_datetime("2024-05-31"))
+
+
 #get data on this ticker
-tickerData = yf.Ticker(tickerSymbol)
+tickerData = yf.Ticker(upper(tickerSymbol))
+
 #get the historical prices for this ticker
-tickerDf = tickerData.history(period= '1d', start = '2014-05-31', end = '2024-5-31')
-# Open High  Low Close  Volume   Dividends  Stock Splits
+tickerDf = tickerData.history(period= '1d', start = start_date, end = end_date)
+# Open, High, Low, Close, Volume, Dividends,  Stock Splits
 
 
 st.write("""
